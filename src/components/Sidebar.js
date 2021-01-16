f7fed2511be3156d140a8d6360a785254104d571
medxkh <medkh6699@gmail.com>
2021-01-16 20:11:19 +0100

    style sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,31 +1,39 @@
 import React from "react";
+import facebook from "../assets/icons/facebook.svg";
+import github from "../assets/icons/github.svg";
+import pin from "../assets/icons/pin.svg";
+import tie from "../assets/icons/tie.svg";
+import mightycoder from "../assets/mightycoder.svg";
 
 function Sidebar() {
   return (
-    <div>
-      <img src="" alt="avatar" className="sidebar__avatar" />
+    <div className="sidebar">
+      <img src={mightycoder} alt="avatar" className="sidebar__avatar" />
       <div className="sidebar__name">
         Med <span>Kh </span>
       </div>
       <div className="sidebar__item sidebar__title"> Web Developer</div>
-      <a href="">
-        <div className="sidebar__item">
-          <img src="" alt="resume" className="sidebar__icon" />
+      <a href="!#">
+        <div className="sidebar__item sidebar__resume">
+          <img src={tie} alt="resume" className="sidebar__icon" />
           Download Resume
         </div>
       </a>
-      <figure className="sidebar__social-icons">
+      <figure className="sidebar__social-icons my-2">
         <a href="">
-          <img src="" alt="facebook" className="sidebar__icon" />
-          <img src="" alt="gmail" className="sidebar__icon" />
+          <img src={facebook} alt="facebook" className="sidebar__icon mr-3" />
         </a>
       </figure>
-      <div className="contact">
-        <div className="sidebar__item">
+      <div className="sidebar__contact">
+        <div className="sidebar__item sidebar__github mr-3">
           <a href="">
-            <img src="" alt="github" className="sidebar__icon" />
+            <img src={github} alt="github" className="sidebar__icon" />
             github
           </a>
+        </div>
+        <div className="sidebar__location">
+          <img src={pin} alt="location" className="sidebar__icon" />
+          Tunis,Manar
         </div>
         <div className="sidebar__item">medkh6699@gmail.com</div>
         <div className="sidebar__item">55436765</div>
