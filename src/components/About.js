1f1b06febf76d88f8fde11b8f894a4202eb35cfe
medxkh <medkh6699@gmail.com>
2021-01-18 22:11:58 +0100

    add skillscard

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,36 @@
 import React from "react";
+import api from "../assets/icons/api.svg";
+import Skillcard from "./Skillcard";
+
+const skills = [
+  {
+    icon: api,
+    title: "lorem ipsum dolor sit.",
+    about:
+      "lorem of 10 line and this is it zmot sarijio dila moppi cambojiballalala",
+  },
+  {
+    icon: api,
+    title: "lorem ipsum dolor sit.",
+    about:
+      "lorem of 10 line and this is it zmot sarijio dila moppi cambojiballalala",
+  },
+];
 
 function About() {
-  return <div>about</div>;
+  return (
+    <div className="about">
+      <h6 className="about__intro">lorem haykavki,g for the lopmamikaaa</h6>
+      <div className="container about__container">
+        <h6 className="about__heading"></h6>
+        <div className="row">
+          {skills.map((skill) => (
+            <Skillcard skill={skill} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
