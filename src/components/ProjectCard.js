465faa772f539e9820464282438260863db73152
medxkh <medkh6699@gmail.com>
2021-01-21 23:33:21 +0100

    add projectCard

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import github from "../assets/icons/github.svg";
+
+function ProjectCard({ project: { name, image, deployed_url, github_url } }) {
+  return (
+    <div className="projectCard col-md-6 col-lg-4">
+      <figure className="projectCard__wrapper">
+        <a href={deployed_url} target="_blank">
+          <img src={image} alt={name} className="projectCard__image" />
+        </a>
+        <div className="projectCard__title">
+          <a href={github_url} target="_blank">
+            <img
+              src={github}
+              alt="github link"
+              className="projectCard__image"
+            />
+          </a>
+        </div>
+      </figure>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,17 @@
 import React from "react";
+import data_projects from "./data/projects_data";
+import ProjectCard from "./ProjectCard";
 
 function Projects() {
-  return <div>projects</div>;
+  return (
+    <div className="container projects">
+      <div className="row">
+        {data_projects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
